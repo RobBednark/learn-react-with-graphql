7c3700b74439591de392c06947fd11d7e20a01d1
Rob Bednark <rbednark@gmail.com>
2021-12-30 13:02:18 -0800

    Add <ApolloProvider> wrapper component
    - use 'render' instead of ReactDOM.render (per the doc)
    - status: working with no errors or warnings

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,10 @@
+// From: https://www.apollographql.com/docs/react/get-started/
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
 import {
   ApolloClient,
   InMemoryCache,
-  gql
+  ApolloProvider,
 } from "@apollo/client";
 
 const client = new ApolloClient({
@@ -14,26 +12,22 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
-client
-  .query({
-    query: gql`
-      query GetRates {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `
-  })
-  .then(result => console.log(`Rob Bednark: the result from the query is:`, result));
+// Q: Where is this App() function getting called?
+function App() {
+    return (
+      <div>
+        <h2>Rob Bednark: my Apollo &lt;App&gt; component</h2>
+      </div>
+    );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
+/*
+"You connect Apollo Client to React with the ApolloProvider component. Similar to React's Context.Provider, ApolloProvider wraps your React app and places Apollo Client on the context, which enables you to access it from anywhere in your component tree."
+*/
+
+render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
